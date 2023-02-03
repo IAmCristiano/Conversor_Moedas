e68b6f2a2ed20507acd675054cdd7f34bcdcd6e7
IAmCristiano <cristiano123@gmail.com>
2023-02-03 05:21:45 -0300

    Converter também com Enter, código mais organizado

--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@ Conversor simples, feito para praticar com HTML, CSS e JavaScript.
 
 ## Funcionamento:
 
-O usuário deve selecionar entre Real, Dólar ou Euro. Após isso, inserir o valor desejado e realizar a conversão. Se clicar no botão "Inverter", trocam os valores invertidos. Se estava convertendo de dólar para real, passa a converter de real para dólar.
+O usuário deve selecionar entre Real, Dólar ou Euro. Após isso, inserir o valor desejado e realizar a conversão clicando no botão "Converter", ou pressionando a tecla "Enter". Se clicar no botão "Inverter", trocam os valores invertidos. Se estava convertendo de dólar para real, passa a converter de real para dólar.
 Foi utilizada a ExchangeRate-API para obter os valores atualizados das moedas.
 
 Feito por Cristiano.
--- script.js
+++ script.js
@@ -1,31 +1,49 @@
-const endpoint = `https://api.exchangerate-api.com/v4/latest/USD`;
+class conversorDeMoedas {
+    constructor() {
+        this.endpoint = `https://api.exchangerate-api.com/v4/latest/USD`;
+        this.inputValor = document.getElementById("input-valor");
+        this.moedaDe = document.getElementById("moedaDe");
+        this.moedaPara = document.getElementById("moedaPara");
+        this.resultadoSpan = document.querySelector("#resultado span");
+        this.botaoConverter = document.getElementById("converter");
+        this.botaoInverter = document.getElementById("inverter");
+        this.setupEventListeners();
+    }
 
-const fetchExchangeRate = async (moedaDe, moedaPara) => {
-    const response = await fetch(endpoint);
-    const data = await response.json();
-    const taxaDeCambio = data.rates[moedaPara] / data.rates[moedaDe];
-    return taxaDeCambio;
-};
+    async fetchExchangeRate(moedaDe, moedaPara) {
+        const response = await fetch(this.endpoint);
+        const data = await response.json();
+        const taxaDeCambio = data.rates[moedaPara] / data.rates[moedaDe];
+        return taxaDeCambio;
+    }
 
-const converter = async () => {
-    const valor = document.getElementById("valor").value;
-    const moedaDe = document.getElementById("moedaDe").value;
-    const moedaPara = document.getElementById("moedaPara").value;
-    const taxaDeCambio = await fetchExchangeRate(moedaDe, moedaPara);
-    const resultado = valor * taxaDeCambio;
-    document.querySelector("#resultado span").innerHTML = resultado.toFixed(2);
-    document.getElementById("valor").focus();
-};
+    async converter() {
+        const valor = this.inputValor.value;
+        const moedaDe = this.moedaDe.value;
+        const moedaPara = this.moedaPara.value;
+        const taxaDeCambio = await this.fetchExchangeRate(moedaDe, moedaPara);
+        const resultado = valor * taxaDeCambio;
+        this.resultadoSpan.innerHTML = resultado.toFixed(2);
+        this.inputValor.focus();
+    }
 
-const inverter = () => {
-    const moedaDe = document.getElementById("moedaDe");
-    const moedaPara = document.getElementById("moedaPara");
-    const moedaDeSelection = moedaDe.value;
-    moedaDe.value = moedaPara.value;
-    moedaPara.value = moedaDeSelection;
-    converter();
-};
+    inverter() {
+        const moedaDeSelection = this.moedaDe.value;
+        this.moedaDe.value = this.moedaPara.value;
+        this.moedaPara.value = moedaDeSelection;
+        this.converter();
+    }
 
-document.getElementById("valor").focus();
-document.getElementById("converter").addEventListener("click", converter);
-document.getElementById("inverter").addEventListener("click", inverter);
+    setupEventListeners() {
+        this.inputValor.focus();
+        this.botaoConverter.addEventListener("click", () => this.converter());
+        this.botaoInverter.addEventListener("click", () => this.inverter());
+        this.inputValor.addEventListener("keyup", (event) => {
+            if (event.key === "Enter") {
+                this.converter();
+            }
+        });
+    }
+}
+
+const conversor = new conversorDeMoedas();
